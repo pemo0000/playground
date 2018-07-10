0ce98ce6669d33f71ffa211f00eafdb49f82c03a
pemo0000 <pemo0000@gmx.de>
2018-07-10 18:49:43 +0200

    better or even worse?!?

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse, string
 print("Running...\n")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-b", "--boardsize", help="integer [1-26] to define the boardsize to be drawn", type=int, choices=range(1,27))
+parser.add_argument("-b", "--boardsize", help="integer [1-26] to define the boardsize to be drawn. Default is 8 -> chessboard.", type=int, choices=range(1,27), default=8)
 args = parser.parse_args()
 
 class Board:
@@ -16,21 +16,20 @@ class Board:
         self.x = []
         for i in range(0,n):
             tempboard = []
+            tempboard.extend(str(args.boardsize))
+            args.boardsize -= 1
             for j in range(0,n):
                 if i & 1 and j & 1 or not i & 1 and not j & 1:
-                    tempboard += ['X'] 
+                    tempboard += [' '] 
                 else:
-                    tempboard += [' ']
+                    tempboard += ['X']
             self.board += [tempboard]
             self.x += self.alphabet[i]
         self.n=n
 
     def print(self):
         for i in range(0,self.n):
-            print(str(args.boardsize) + "\t", end = '')
-            args.boardsize -= 1
             print(*self.board[i], sep = '')
-        print("\t", end ='')
         print(*self.x, sep = '')
 
 board1 = Board(args.boardsize)
