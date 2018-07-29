aad59f8098c490f3a58f3f6df4fdb5cd19af02fc
pemo0000 <pemo0000@gmx.de>
2018-07-29 22:30:19 +0200

    doku DB.py

--- DB.py
+++ DB.py
@@ -4,11 +4,13 @@ import os
 class DB:
 
     def __init__(self):
+        """Constructor creating a database, a connection and a cursor"""
         self.createDB()
         self.connection = sqlite3.connect("playground.db")
         self.cursor = self.connection.cursor()
 
     def createDB(self):
+        """Is creating a database if not already there"""
         if not os.path.exists("playground.db"):
             print("Datenbank playground.db nicht vorhanden - Datenbank wird anglegt...")
             self.connection = sqlite3.connect("playground.db")
@@ -18,9 +20,12 @@ class DB:
             print("Datenbank playground.db angelegt.")
 
     def insertFEN(self, fen):
-        sql = "insert into FEN values(?);"
-        self.cursor.execute(sql, [fen])
+        """Is inserting FEN into sqlite if script is called with a FEN and -s
+        :param fen: chess position in Forsyth-Edwards-Notation (FEN), e.g. r4rnk/1pp4p/3p4/3P1b2/1PPbpBPq/8/2QNB1KP/1R3R2 w KQkq - 0 25
+        """
+        self.cursor.execute("insert into FEN values(?);", [fen])
 
     def __del__(self):
+        """Destructor"""
         self.connection.commit()
         self.connection.close()
