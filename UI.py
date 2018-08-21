673518fa2158cb31898e0e2c464ba9390488ed56
pemo0000 <pemo0000@gmx.de>
2018-08-21 21:56:19 +0200

    strulls wxboard wunderbar

--- UI.py
+++ UI.py
@@ -6,6 +6,7 @@ class View(wx.Panel):
           'FEN':     {'darkSquareColor':'grey'}
                   }
   def __init__(self, parent):
+    self.parent = parent 
     super(View, self).__init__(parent)
     self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
     self.Bind(wx.EVT_SIZE, self.on_size)
@@ -17,10 +18,10 @@ class View(wx.Panel):
     self.Refresh()
 
   def on_paint(self, event):
-    w, h = self.GetClientSize()
+    w, h = self.parent.GetClientSize()
     dc = wx.AutoBufferedPaintDC(self)
     dc.Clear()
-    rectangleSize = h/self.boardsize
+    rectangleSize = min(h, w)/self.boardsize
     x=y=0
     for i in range(0,self.boardsize):
       for j in range(0,self.boardsize):
@@ -41,26 +42,3 @@ class Frame(wx.Frame):
     self.SetClientSize((500, 500))
     self.Center()
     self.view = View(self)
-    self.CreateStatusBar()
-    self.SetStatusText("Statusbar")
-    filemenu= wx.Menu()
-    menuAbout= filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
-    menuExit = filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
-    menuBar = wx.MenuBar()
-    menuBar.Append(filemenu,"&File")
-    self.SetMenuBar(menuBar)
-    self.Centre() 
-    self.Show(True)
-    self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
-    self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
-    # Drawing test images...
-    bmp = wx.Bitmap("p40.png")
-    wx.StaticBitmap(self, bitmap=bmp, pos=(58,55))
-
-  def OnAbout(self,e):
-     dlg = wx.MessageDialog(self, "Maybe the Ultimate Chess Playground.\nDevelopped by E8 under strong code and design control of E1.", "About the Ultimate Chess Playground", wx.OK)
-     dlg.ShowModal()
-     dlg.Destroy()
-
-  def OnExit(self,e):
-      self.Close(True)
